ba6ad47a517300c0db394bae3a93cf7781391198
Kewin Jechu <kewin@example.com>
2025-05-07 09:23:44 -0500

    Update connection_db.py with real PostgreSQL credentials

--- connection_db.py
+++ connection_db.py
@@ -7,10 +7,17 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
+# Obtener variables de entorno con valores por defecto para pruebas locales
+POSTGRESQL_ADDON_USER = os.getenv('POSTGRESQL_ADDON_USER', 'uupit9mkwkdby2wuqimw')
+POSTGRESQL_ADDON_PASSWORD = os.getenv('POSTGRESQL_ADDON_PASSWORD', 'WLbqR3mqLfxJVbLZRMmds0FVcjPgrd')
+POSTGRESQL_ADDON_HOST = os.getenv('POSTGRESQL_ADDON_HOST', 'bxmi248k1opffz8lupql-postgresql.services.clever-cloud.com')
+POSTGRESQL_ADDON_PORT = os.getenv('POSTGRESQL_ADDON_PORT', '5432')  # Usamos 5432 como estándar
+POSTGRESQL_ADDON_DB = os.getenv('POSTGRESQL_ADDON_DB', 'bxmi248k1opffz8lupql')
+
 DATABASE_URL = (
-    f"postgresql+asyncpg://{os.getenv('POSTGRESQL_ADDON_USER')}:{os.getenv('POSTGRESQL_ADDON_PASSWORD')}"
-    f"@{os.getenv('POSTGRESQL_ADDON_HOST')}:{os.getenv('POSTGRESQL_ADDON_PORT')}/"
-    f"{os.getenv('POSTGRESQL_ADDON_DB')}"
+    f"postgresql+asyncpg://{POSTGRESQL_ADDON_USER}:{POSTGRESQL_ADDON_PASSWORD}"
+    f"@{POSTGRESQL_ADDON_HOST}:{POSTGRESQL_ADDON_PORT}/"
+    f"{POSTGRESQL_ADDON_DB}"
 )
 
 engine = create_async_engine(DATABASE_URL, echo=True)
