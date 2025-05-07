74bb947fd7f665922f8b71721fa1a50afc19ded7
Kewin Jechu <kewin@example.com>
2025-05-07 09:25:51 -0500

    Update connection_db.py with correct port and SSL mode

--- connection_db.py
+++ connection_db.py
@@ -7,17 +7,17 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-# Obtener variables de entorno con valores por defecto para pruebas locales
+# Obtener variables de entorno con valores por defecto
 POSTGRESQL_ADDON_USER = os.getenv('POSTGRESQL_ADDON_USER', 'uupit9mkwkdby2wuqimw')
 POSTGRESQL_ADDON_PASSWORD = os.getenv('POSTGRESQL_ADDON_PASSWORD', 'WLbqR3mqLfxJVbLZRMmds0FVcjPgrd')
 POSTGRESQL_ADDON_HOST = os.getenv('POSTGRESQL_ADDON_HOST', 'bxmi248k1opffz8lupql-postgresql.services.clever-cloud.com')
-POSTGRESQL_ADDON_PORT = os.getenv('POSTGRESQL_ADDON_PORT', '5432')  # Usamos 5432 como estándar
+POSTGRESQL_ADDON_PORT = os.getenv('POSTGRESQL_ADDON_PORT', '50013')  # Actualizado a 50013
 POSTGRESQL_ADDON_DB = os.getenv('POSTGRESQL_ADDON_DB', 'bxmi248k1opffz8lupql')
 
 DATABASE_URL = (
     f"postgresql+asyncpg://{POSTGRESQL_ADDON_USER}:{POSTGRESQL_ADDON_PASSWORD}"
     f"@{POSTGRESQL_ADDON_HOST}:{POSTGRESQL_ADDON_PORT}/"
-    f"{POSTGRESQL_ADDON_DB}"
+    f"{POSTGRESQL_ADDON_DB}?sslmode=require"  # Añadimos sslmode=require
 )
 
 engine = create_async_engine(DATABASE_URL, echo=True)
